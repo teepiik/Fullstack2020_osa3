68815fcf3a770dea56a4834c1e28c31f313c3b26
teepiik <teemu.piik@gmail.com>
2019-12-12 14:12:38 +0200

    frontend build to heroku

---
+++ build/precache-manifest.5b2ef81a5481a74c9b60f14c58cc5790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b0f49c6ff2981aef61f27cfc3b852a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "859830a7ac5cca051381",
+    "url": "/static/css/main.27eb2bdd.chunk.css"
+  },
+  {
+    "revision": "332e94772b7fef02148c",
+    "url": "/static/js/2.923ab7fc.chunk.js"
+  },
+  {
+    "revision": "859830a7ac5cca051381",
+    "url": "/static/js/main.efa47cbd.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
