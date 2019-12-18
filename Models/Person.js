96b22fdd6017c20f0d22dba5722e9b93548e0fc7
teepiik <teemu.piik@gmail.com>
2019-12-18 13:51:29 +0200

    3.22 Linting

--- Models/Person.js
+++ Models/Person.js
@@ -11,26 +11,25 @@ mongoose.connect(url, { useNewUrlParser: true })
         console.log('Error connecting to MONGODB: ', error.message)
     })
 
-    const personSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: true,
-            minlength: 3
-        },
-        number: {
-            type: String,
-            required: true,
-            minlength: 8
-        }
-    })
-    personSchema.plugin(uniqueValidator)
-    
-    personSchema.set('toJSON', {
-        transform: (document, returnedObject) => {
-            returnedObject.id = returnedObject._id.toString()
-            delete returnedObject._id
-            delete returnedObject.__v
-        }
-    })
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+})
+personSchema.plugin(uniqueValidator)
 
-    module.exports = mongoose.model('Person', personSchema)
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+module.exports = mongoose.model('Person', personSchema)
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/api/info', (req, res) => {
     Person.find({})
         .then(persons => {
             res.send(`<h2>Phonebook has info for ${persons.length} people.</h2><h2>${timestamp}</h2>`)
-        })  
+        })
 })
 
 app.get('/api/', (req, res) => {
@@ -38,7 +38,7 @@ app.get('/api/persons/:id', (req, res, next) => {
                 res.json(person.toJSON())
             } else {
                 res.status(204).end()
-            } 
+            }
         })
         .catch(error => next(error))
 })
@@ -47,23 +47,16 @@ app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if(!body.name) {
-        return res.status(400).json({ error: "Name is missing."})
+        return res.status(400).json({ error: 'Name is missing.' })
     }
 
     if(!body.number) {
-        return res.status(400).json({ error: "Number is missing."})
+        return res.status(400).json({ error: 'Number is missing.' })
     }
-    
-    /*
-    const names = persons.map(person => person.name)
-    if(names.includes(body.name)) {
-        return res.status(400).json( {error: "Name already in phonebook."})
-    }*/
 
     const person = new Person({
         name: body.name,
         number: body.number,
-        //id: Math.floor(Math.random() * 100000000)
     })
 
     person.save()
@@ -81,13 +74,12 @@ app.put('/api/persons/:id', (req, res, next) => {
         number: body.number
     }
 
-    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
         .then(updatedPerson => {
             res.json(updatedPerson.toJSON())
         })
         .catch(error => next(error))
-}) 
-    
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
@@ -100,10 +92,10 @@ app.delete('/api/persons/:id', (req, res, next) => {
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
 
-    if(error.name === "CastError" && error.kind == "ObjectId") {
-        return res.status(400).send({error: 'Bad id format'})
+    if(error.name === 'CastError' && error.kind === 'ObjectId') {
+        return res.status(400).send({ error: 'Bad id format' })
     } else if(error.name === 'ValidationError') {
-        return res.status(400).json({error: error.message})
+        return res.status(400).json({ error: error.message })
     }
     next(error)
 }
